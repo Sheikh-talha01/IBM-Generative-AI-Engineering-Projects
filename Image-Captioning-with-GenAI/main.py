fa504bf2f880e1eef38a72290c3fedd6c22cd3ba
Talha Sheikh <167472327+Sheikh-talha01@users.noreply.github.com>
2025-12-29 22:43:25 +0500

    deleted extra file and made a folder for BLIP-image-captioning-project

--- Image-Captioning-with-GenAI/main.py
+++ Image-Captioning-with-GenAI/main.py
@@ -5,6 +5,8 @@ import gradio as gr
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
+
+#made a function
 def generate_caption(image):
     inputs = processor(images=image, return_tensors="pt")
     out = model.generate(**inputs)
